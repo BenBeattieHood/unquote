f1fa5fd5f0208865dccaaa15cf8e47dd804cf5f0
BenBeattieHood <benb@pageuppeople.com>
2017-05-30 08:57:04 +1000

    Added build for .net 4.6.2 / F# 4.1

--- Unquote/AssemblyInfo.fs
+++ Unquote/AssemblyInfo.fs
@@ -41,5 +41,5 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 //use odd number revision number (fourth position) to indicate pre-release
-[<assembly: AssemblyVersion("3.2.0.0")>]
+[<assembly: AssemblyVersion("3.3.0.0")>]
 ()
